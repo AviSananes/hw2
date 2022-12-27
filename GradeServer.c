6b88bd03ec8c29962a31e90baa084a8a3efa178e
Avi Sananes <avi.sananes@aidoc.com>
2022-12-27 19:56:58 +0200

    added things to server

--- GradeServer.c
+++ GradeServer.c
@@ -21,23 +21,33 @@ void* handle_connection(void* arg);
 void sigint_handler(int sig);
 
 
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s <port>\n", argv[0]);
     return 1;
   }
-  
-  struct sigaction action;
-  memset(&action, 0, sizeof(action));
-  action.sa_handler = sigint_handler;
-  sigaction(SIGINT, &action, NULL);
 
+  struct sigaction sa;
+  memset(&sa, 0, sizeof(sa));
+  sa.sa_handler = sigint_handler;
+  sigaction(SIGINT, &sa, NULL);
+
+  // Initialize the task list and synchronization variables
+  task_list = malloc(sizeof(int) * NUM_THREADS);
+  if (task_list == NULL) {
+    perror("Error allocating memory for task list");
+    return 1;
+  }
+  task_list_size = 0;
+  pthread_mutex_init(&task_list_mutex, NULL);
+  pthread_cond_init(&task_list_cond, NULL);
 
   // Get the port number from the command line argument
   int port = atoi(argv[1]);
 
   // Create a socket
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if (sockfd < 0) {
     perror("Error opening socket");
     return 1;
@@ -62,32 +72,84 @@ int main(int argc, char* argv[]) {
   // Create a pool of worker threads
   pthread_t threads[NUM_THREADS];
   for (int i = 0; i < NUM_THREADS; i++) {
-    int* connfd = malloc(sizeof(int));
-    if (connfd == NULL) {
-      perror("Error allocating memory for connection descriptor");
-      return 1;
-    }
-
-    // Accept a new connection and assign it to a worker thread
-    *connfd = accept(sockfd, NULL, NULL);
-    if (*connfd < 0) {
-      perror("Error accepting connection");
-      return 1;
-    }
-    if (pthread_create(&threads[i], NULL, handle_connection, connfd) != 0) {
+    if (pthread_create(&threads[i], NULL, handle_connection, NULL) != 0) {
       perror("Error creating thread");
       return 1;
     }
   }
+    while (1) {
+    // Accept a connection
+    struct sockaddr_in cli_addr;
+    socklen_t cli_addr_len = sizeof(cli_addr);
+    int connfd = accept(sockfd, (struct sockaddr*) &cli_addr, &cli_addr_len);
+    if (connfd < 0) {
+      perror("Error accepting connection");
+      return 1;
+    }
 
-  // Wait for all threads to finish
+    // Add the task (connection) to the task list
+    pthread_mutex_lock(&task_list_mutex);
+    task_list[task_list_size++] = connfd;
+    pthread_cond_signal(&task_list_cond);
+    pthread_mutex_unlock(&task_list_mutex);
+  }
+    // Wait for the worker threads to finish
   for (int i = 0; i < NUM_THREADS; i++) {
     pthread_join(threads[i], NULL);
   }
 
   // Close the socket
   close(sockfd);
+
+  // Free memory and destroy synchronization variables
+  free(task_list);
+  pthread_mutex_destroy(&task_list_mutex);
+  pthread_cond_destroy(&task_list_cond);
+
   return 0;
+}
+
+
+
+
+void* handle_connection(void* arg) {
+  while (1) {
+    // Take a task from the task list
+    int connfd;
+    pthread_mutex_lock(&task_list_mutex);
+    while (task_list_size == 0) {
+      pthread_cond_wait(&task_list_cond, &task_list_mutex);
+    }
+    connfd = task_list[--task_list_size];
+    pthread_mutex_unlock(&task_list_mutex);
+
+    // Read input from the client
+    char buffer[256];
+    int n = read(connfd, buffer, 255);
+    if (n < 0) {
+      perror("Error reading from socket");
+      return NULL;
+    }
+    buffer[n] = '\0';
+
+    // Handle the request here.
+    // For example, you may parse the request to determine the type of operation being requested
+    // and perform the appropriate action.
+    // ...
+
+    // Send a response back to the client
+    char* response = "Request received and processed.";
+    n = write(connfd, response, strlen(response));
+    if (n < 0) {
+      perror("Error writing to socket");
+      return NULL;
+    }
+
+    // Close the connection
+    close(connfd);
+  }
+
+  return NULL;
 }
 
 
@@ -133,27 +195,6 @@ void separate_strings(char* input, char* first, char* second, char* third) {
 
   // If no space is found in the second string, set the third string to an empty string
   third[0] = '\0';
-}
-
-
-void* handle_connection(void* arg) {
-  while (1) {
-    // Take a task from the task list
-    int connfd;
-    pthread_mutex_lock(&task_list_mutex);
-    while (task_list_size == 0) {
-      pthread_cond_wait(&task_list_cond, &task_list_mutex);
-    }
-    connfd = task_list[--task_list_size];
-    pthread_mutex_unlock(&task_list_mutex);
-
-    // Handle the connection here.
-    // For example, you may read data from the connection and send a response back.
-    // ...
-
-    close(connfd);
-  }
-  return NULL;
 }
 
 void sigint_handler(int sig) {
